23b7e842babcb42f0475e7cccb0fd7d45dd2cf97
Simon Bergot <simon.bergot@gmail.com>
2013-09-08 23:57:58 +0200

    minor cleaning

--- src/System/Console/EasyConsole/ArgsProcess.hs
+++ src/System/Console/EasyConsole/ArgsProcess.hs
@@ -1,16 +1,6 @@
-module System.Console.EasyConsole.ArgsProcess (
-  preprocess,
-  Arg,
-  Args,
-  NiceArgs,
-  ArgConsumer,
-  takePos,
-  takeAllPos,
-  takeFlag
-  ) where
+module System.Console.EasyConsole.ArgsProcess (preprocess) where
 
-
-import           System.Console.EasyConsole.BaseType hiding (ArgSrc(..)) 
+import           System.Console.EasyConsole.BaseType  
 import qualified Data.Map as M
 import Data.List
 
@@ -25,42 +15,23 @@ getWord (Pos word) = word
 getWord (Flag word) = word
 
 tokenize :: Args -> [Token]
-tokenize = concatMap arg2token
-  where
+tokenize = concatMap arg2token where
   arg2token :: Arg -> [Token]
   arg2token ('-':'-':word) = [Flag word] 
   arg2token ('-':word) =  map (Flag . (:[]) ) word 
   arg2token word = [Pos word] 
 
 collectPos :: [Token] -> (Args, [Token])
-collectPos tokens = (pos, rest)
-  where
+collectPos tokens = (pos, rest) where
   (posargs, rest) = span isPos tokens
   pos = map getWord posargs
 
 parseFlag :: [Token] -> Maybe ((Arg, Args), [Token])
-parseFlag (Flag word : tokens) = Just ((word, args), rest)
-  where
+parseFlag (Flag word : tokens) = Just ((word, args), rest) where
   (args, rest) = collectPos tokens
 parseFlag _ = Nothing
 
 preprocess :: Args -> NiceArgs
-preprocess args = (pos, flagArgs)
-  where
+preprocess args = (pos, flagArgs) where
   (pos, rest) =  collectPos $ tokenize args
   flagArgs = M.fromList $ unfoldr parseFlag rest
-  
-
-type ArgConsumer = NiceArgs -> (Maybe Args, NiceArgs)  
-
-takePos :: ArgConsumer 
-takePos args@([], _) = (Nothing, args)
-takePos (x:xs, flags) = (Just [x], (xs, flags)) 
-
-takeAllPos :: ArgConsumer
-takeAllPos (args, flags) = (Just args, ([], flags)) 
-
-takeFlag :: String -> ArgConsumer
-takeFlag key fullargs@(args, flags) = case M.lookup key flags of
-  Nothing       -> (Nothing, fullargs)
-  Just flagargs -> (Just flagargs, (args, M.delete key flags))
