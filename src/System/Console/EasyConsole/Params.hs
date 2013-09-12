ec529c709e2c78bf98953d8025861945db477228
sberg <sbergot@efront.com>
2013-09-12 14:05:43 +0200

    merge multi and single arg definitions

--- src/System/Console/EasyConsole/Params.hs
+++ src/System/Console/EasyConsole/Params.hs
@@ -1,4 +1,13 @@
-module System.Console.EasyConsole.Params where
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module System.Console.EasyConsole.Params (
+  FlagParam,
+  Descr (..),
+  StdArgParam,
+  ArgSrc,
+  Optionality (..)
+) where
 
 import qualified Data.Map                            as M
 import           System.Console.EasyConsole.BaseType
@@ -31,16 +40,57 @@ instance ParamSpec spec => ParamSpec (Descr spec) where
 
 data ArgSrc = Flag | Pos
 
+data Optionality a = Mandatory | Optional a
+
+class ParserArg argformat where
+  runflagparse :: (argformat -> res) -> Args -> ParseResult res
+  runposparse :: (argformat -> res) -> Args -> (ParseResult res, Args)
+  getvalformat :: (argformat -> res) -> String
+
+instance ParserArg Arg where
+  runflagparse _        [] = Left "missing arg"
+  runflagparse parser [val] = Right $ parser val
+  runflagparse _        _     = Left "too many args"
+
+  runposparse _ [] = (Left "missing arg", [])
+  runposparse parser (val:rest) = (Right $ parser val, rest)
+
+  getvalformat _ = "VAL"
+
+instance ParserArg Args where
+  runflagparse parser vals = Right $ parser vals
+  runposparse  parser vals = (Right $ parser vals, [])
+  getvalformat _ = "VAL [VALS ...]"
+
+data StdArgParam argformat a =  StdArgParam (Optionality a) ArgSrc String (argformat -> a)
+
+instance ParserArg argformat => ParamSpec (StdArgParam argformat) where
+  getparser (StdArgParam opt src key parse) = Parser rawparse where
+    rawparse = choosesrc flagparse posparse src
+    defaultOrError = missing opt
+
+    flagparse (pos, flags) = (logkey key res, (pos, M.delete key flags)) where
+      res = case M.lookup key flags of
+        Nothing -> defaultOrError "missing flag"
+        Just args -> runflagparse parse args
+
+    posparse (pos, flags) = case pos of
+      [] -> (logkey key $ defaultOrError "missing arg", (pos, flags))
+      args -> let (res, rest) = runposparse parse args
+              in  (res, (rest, flags))
+
+  getcategory (StdArgParam opt _ _ _) = category opt
+  getargformat (StdArgParam _ src key parser) =
+   getkeyformat src key ++ "  " ++ getvalformat parser
+
 choosesrc :: a -> a -> ArgSrc -> a
 choosesrc flag pos src = case src of
   Flag -> flag
   Pos -> pos
 
-argformat :: ArgSrc -> String -> String
-argformat Pos key = key ++ "VAL"
-argformat Flag key = flagformat key ++ " VAL"
-
-data Optionality a = Mandatory | Optional a
+getkeyformat :: ArgSrc -> String -> String
+getkeyformat Pos key = key
+getkeyformat Flag key = flagformat key
 
 missing :: Optionality a -> String -> ParseResult a
 missing Mandatory msg = Left msg
@@ -53,41 +103,3 @@ category _         = "optional"
 logkey :: String -> ParseResult a -> ParseResult a
 logkey key (Left err) = Left $ "fail to parse " ++ key ++ " : " ++ err
 logkey _   val = val
-
-data SingleArgParam a = SingleArgParam (Optionality a) ArgSrc String (Arg -> a)
-
-instance ParamSpec SingleArgParam where
-  getparser (SingleArgParam opt src key parse) = Parser rawparse where
-    rawparse = choosesrc flagparse posparse src
-    defaultOrError = missing opt
-
-    flagparse (pos, flags) = (logkey key res, (pos, M.delete key flags)) where
-      res = case M.lookup key flags of
-        Nothing -> defaultOrError "missing flag"
-        Just [] -> Left "missing arg"
-        Just [val] -> Right $ parse val
-        Just _ -> Left "too many args"
-
-    posparse (pos, flags) = case pos of
-      [] -> (logkey key $ defaultOrError "missing arg", (pos, flags))
-      val:rest -> (Right $ parse val, (rest, flags))
-
-  getcategory (SingleArgParam opt _ _ _) = category opt
-  getargformat (SingleArgParam _ src key _) = argformat src key
-
-data MultipleArgParam a = MultipleArgParam (Optionality a) ArgSrc String (Args -> a)
-
-instance ParamSpec MultipleArgParam where
-  getparser (MultipleArgParam opt src key parse) = Parser rawparse where
-    rawparse = choosesrc flagparse posparse src
-    defaultOrError = missing opt
-
-    flagparse (pos, flags) = (logkey key res, (pos, M.delete key flags)) where
-      res = case M.lookup key flags of
-        Nothing -> defaultOrError "missing flag"
-        Just vals -> Right $ parse vals
-
-    posparse (pos, flags) = (Right $ parse pos, ([], flags))
-
-  getcategory (MultipleArgParam opt _ _ _) = category opt
-  getargformat (MultipleArgParam _ src key _) = argformat src key
