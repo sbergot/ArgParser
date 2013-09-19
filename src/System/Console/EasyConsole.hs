15b56586bccaaf38e7c2fca0e1bb65f4b6bc5ae7
sberg <sbergot@efront.com>
2013-09-19 14:54:33 +0200

    updated package to export subparser functionality

--- src/System/Console/EasyConsole.hs
+++ src/System/Console/EasyConsole.hs
@@ -1,7 +1,8 @@
 module System.Console.EasyConsole (module X) where
-import           System.Console.EasyConsole.BaseType as X
-import           System.Console.EasyConsole.Params   as X
-import           System.Console.EasyConsole.Parser   as X
-import           System.Console.EasyConsole.Run      as X
+import           System.Console.EasyConsole.BaseType  as X
+import           System.Console.EasyConsole.Params    as X
+import           System.Console.EasyConsole.Parser    as X
+import           System.Console.EasyConsole.Run       as X
+import           System.Console.EasyConsole.SubParser as X
 
--- TODO subparsers
+-- TODO documentation
