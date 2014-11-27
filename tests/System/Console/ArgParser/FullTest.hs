8bc2c80a7a69cfa5e38f1e31290cd3d201428a81
Simon Bergot <sbergot@talentsoft.com>
2014-11-27 15:35:30 +0100

    added failing tests case

---
+++ tests/System/Console/ArgParser/FullTest.hs
@@ -0,0 +1,52 @@
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+module System.Console.ArgParser.FullTest where
+import System.Console.ArgParser.Parser
+import System.Console.ArgParser.Run
+
+import System.Console.ArgParser.BaseType
+import System.Console.ArgParser.QuickParams
+import System.Console.ArgParser.TestHelpers
+
+import Test.Framework
+import qualified Test.HUnit as H
+
+{-# ANN module "HLint: ignore Use camelCase" #-}
+
+data MyTest = MyTest Bool Bool [String]
+    deriving (Show, Eq)
+
+myTestParser :: ParserSpec MyTest
+myTestParser = MyTest
+    `parsedBy` boolFlag "common-mode"
+    `andBy`    boolFlag "docbook-mode"
+    `andBy`    posArgs  "files" [] (\xs x -> xs ++ [x])
+
+
+myTestApp :: CmdLnInterface MyTest
+myTestApp = mkDefaultApp myTestParser "MyTestParser"
+
+testparse :: [String] -> ParseResult MyTest
+testparse args = parseArgs args myTestApp
+
+checkParse :: [String] -> MyTest -> H.Assertion
+checkParse args expected = case testparse args of
+    (Right result) -> assertEqual expected result
+    (Left msg) -> assertFailure $ "parse failed with: " ++ msg
+
+expectFail :: [String] -> H.Assertion
+expectFail args = case testparse args of
+    Right _ -> assertFailure "expected failure"
+    _ -> return ()
+
+test_base :: H.Assertion
+test_base = checkParse
+    ["1", "2", "3"]
+    (MyTest False False ["1", "2", "3"])
+
+test_pos_after_flag :: H.Assertion
+test_pos_after_flag = checkParse
+    ["--common-mode", "1", "2", "3"]
+    (MyTest True False ["1", "2", "3"])
+
+test_unknown_flag :: H.Assertion
+test_unknown_flag = expectFail ["--unknown", "1", "2", "3"]
--- tests/TestsHTF.hs
+++ tests/TestsHTF.hs
@@ -9,6 +9,7 @@ import {-@ HTF_TESTS @-} System.Console.ArgParser.SubParserTest
 import {-@ HTF_TESTS @-} System.Console.ArgParser.QuickParamsTest
 import {-@ HTF_TESTS @-} System.Console.ArgParser.ParserTest
 import {-@ HTF_TESTS @-} System.Console.ArgParser.ArgsProcessTest
+import {-@ HTF_TESTS @-} System.Console.ArgParser.FullTest
 
 main :: IO()
 main = htfMain htf_importedTests
