d8ea0fe5312b3d9d4c80005890caa881d56207d4
Agustín Mista <mista.agustin@gmail.com>
2017-03-09 19:29:33 -0300

    Fixed error message

--- src/System/Console/ArgParser/Format.hs
+++ src/System/Console/ArgParser/Format.hs
@@ -36,7 +36,7 @@ data CmdLineFormat = CmdLineFormat
 
 -- | Default specification for the help layout
 defaultFormat :: CmdLineFormat
-defaultFormat = CmdLineFormat 75 1 80
+defaultFormat = CmdLineFormat 45 1 50
 
 -- | Prints the application name and version
 showCmdLineVersion :: CmdLnInterface a -> String
